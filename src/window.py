4825c3f18c6bc7aefee6af4f8212575f865f8ad8
Revisto <theRevisto@gmail.com>
2025-08-14 19:56:34 +0330

    Update copyright notices in multiple source files to reflect new ownership (sepehr-rs)

--- src/window.py
+++ src/window.py
@@ -1,6 +1,6 @@
 # window.py
 #
-# Copyright 2025 sepehr
+# Copyright 2025 sepehr-rs
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
