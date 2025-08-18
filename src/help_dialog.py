5c054cab4367130540138b23ba462cb211eef610
Alexander Vanhee <160625516+AlexanderVanhee@users.noreply.github.com>
2025-08-18 15:51:39 +0200

    Make use of adw dialog for help

--- src/help_dialog.py
+++ src/help_dialog.py
@@ -1,27 +1,36 @@
-from gi.repository import Gtk
+# help_dialog.py
+#
+# Copyright 2025 sepehr-rs, Alexander Vanhee
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
 
+from gi.repository import Gtk, Adw
 
-class HowToPlayDialog(Gtk.Dialog):
-    def __init__(self, parent):
-        super().__init__(title="How to Play Sudoku", transient_for=parent, modal=True)
-        self.set_default_size(500, 400)
+class HowToPlayDialog(Adw.Dialog):
+    def __init__(self, parent=None):
+        super().__init__()
+        self.set_title("How to Play Sudoku")
+        self.set_content_width(500)
+        self.set_content_height(400)
 
-        content_area = self.get_content_area()
-        scrolled_window = Gtk.ScrolledWindow()
-        scrolled_window.set_policy(Gtk.PolicyType.AUTOMATIC, Gtk.PolicyType.AUTOMATIC)
-        scrolled_window.set_vexpand(True)
+        toolbar_view = Adw.ToolbarView.new()
+        header = Adw.HeaderBar()
+        toolbar_view.add_top_bar(header)
 
-        content_area.append(scrolled_window)
-
-        textview = Gtk.TextView()
-        textview.get_style_context().add_class("sudoku-dialog")
-        textview.set_editable(False)
-        textview.set_cursor_visible(False)
-        textview.set_wrap_mode(Gtk.WrapMode.WORD_CHAR)
-        textview.set_margin_top(10)
-        textview.set_margin_bottom(10)
-        textview.set_margin_start(10)
-        textview.set_margin_end(10)
+        label = Gtk.Label( margin_top=10, margin_bottom=10, margin_start=10, margin_end=10, wrap=True)
 
         instructions = (
             "Welcome to Sudoku!\n\n"
@@ -35,9 +44,7 @@ class HowToPlayDialog(Gtk.Dialog):
             "- Try to solve the puzzle logically.\n\n"
             "Good luck and have fun!"
         )
-        buffer = textview.get_buffer()
-        buffer.set_text(instructions)
+        label.set_text(instructions)
+        toolbar_view.set_content(label)
+        self.set_child(toolbar_view)
 
-        scrolled_window.set_child(textview)
-
-        self.add_buttons("OK", Gtk.ResponseType.OK)
