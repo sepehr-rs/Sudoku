07a447d254f3be146bf00db9a5d4244b770f1dbd
Alexander Vanhee <160625516+AlexanderVanhee@users.noreply.github.com>
2025-08-20 17:08:27 +0200

    rework how to play

--- src/application.py
+++ src/application.py
@@ -75,7 +75,7 @@ class SudokuApplication(Adw.Application):
 
     def on_how_to_play(self, action, param):
         """Show how to play dialog."""
-        dialog = HowToPlayDialog(self.props.active_window)
+        dialog = HowToPlayDialog()
         dialog.present(self.props.active_window)
 
     def _on_close_request(self, *args):
