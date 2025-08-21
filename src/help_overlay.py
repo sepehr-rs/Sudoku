31450345f7c46b377bb3ceb7e3a60d5471ae99c3
Alexander Vanhee <160625516+AlexanderVanhee@users.noreply.github.com>
2025-08-21 13:47:25 +0200

    fix help overlay

---
+++ src/help_overlay.py
@@ -0,0 +1,25 @@
+# finished_page.py
+#
+# Copyright 2025 sepehr-rs
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+from gi.repository import Gtk
+
+
+@Gtk.Template(resource_path="/io/github/sepehr_rs/Sudoku/blueprints/help-overlay.ui")
+class HelpOverlay(Gtk.ShortcutsWindow):
+    __gtype_name__ = "HelpOverlay"
--- src/window.py
+++ src/window.py
@@ -24,6 +24,7 @@ from .game_board import GameBoard
 from .game_manager import GameManager
 from .ui_helpers import UIHelpers
 from .difficulty_selection_dialog import DifficultySelectionDialog
+from .help_overlay import HelpOverlay
 from .finished_page import FinishedPage  # noqa: F401 Used in Blueprint
 
 
@@ -60,11 +61,18 @@ class SudokuWindow(Adw.ApplicationWindow):
         self.add_controller(gesture)
         action = Gio.SimpleAction.new("show-primary-menu", None)
         action.connect("activate", self.on_show_primary_menu)
+        action = Gio.SimpleAction.new("show-help-overlay", None)
+        action.connect("activate", self.on_show_help_overlay)
         self.add_action(action)
 
     def on_show_primary_menu(self, action, param):
         """Open the hamburger menu popover."""
         self.primary_menu_button.popup()
+
+    def on_show_help_overlay(self, action, param):
+        help_overlay = HelpOverlay()
+        help_overlay.set_transient_for(self)
+        help_overlay.present()
 
     def _get_grid_widget(self):
         frame = self.grid_container.get_first_child()
