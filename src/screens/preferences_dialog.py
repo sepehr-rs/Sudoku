9cab2b8e41070f5a9cd423f3dc5fadaf0f8118e6
sepehrrasooli <sepehrrasouli06@gmail.com>
2025-10-07 16:24:38 +0330

    Fix preferences spacing

--- src/screens/preferences_dialog.py
+++ src/screens/preferences_dialog.py
@@ -8,21 +8,21 @@ class PreferencesDialog(Adw.PreferencesWindow):
         super().__init__(title="Preferences")
         self.set_default_size(600, 550)
         self.set_search_enabled(False)
+
         self.preferences = PreferencesManager.get_preferences()
         if not self.preferences:
             return
+
         page = Adw.PreferencesPage()
-        group = Adw.PreferencesGroup()
-        group.set_margin_bottom(24)
-        page.add(group)
-        group.add(
-            GeneralPreferencesPage(
-                self.preferences.general_defaults, "General Preferences"
-            )
+        general_group = GeneralPreferencesPage(
+            self.preferences.general_defaults,
+            "General Preferences"
         )
-        group.add(
-            VariantPreferencesPage(
-                self.preferences.variant_defaults, self.preferences.name
-            )
+        variant_group = VariantPreferencesPage(
+            self.preferences.variant_defaults,
+            self.preferences.name
         )
+
+        page.add(general_group)
+        page.add(variant_group)
         self.add(page)
